c6d0ef3a6f90d453053206792f7b41e216a8fea6
KimberleyEvans-Parker <kim@evans-parker.com>
2020-03-30 19:00:49 +1300

    rebase - add buttons and format page

--- src/features/dialog-manager/dialogs/character-creation/index.js
+++ src/features/dialog-manager/dialogs/character-creation/index.js
@@ -7,13 +7,6 @@ import Dialog from '../../../../components/dialog';
 import loadStartingItems from '../../../inventory/actions/load-starting-items';
 import showFirstStoryMessage from '../../actions/show-first-story-message';
 
-import ArrowKeys from './assets/arrow-keys.png';
-import DoubleTap from './assets/double-tap.png';
-import Enter from './assets/enter.png';
-import Space from './assets/space.png';
-import Swipe from './assets/swipe.png';
-import WASDKeys from './assets/wasd-keys.png';
-
 import './styles.scss';
 
 const GameInstructions = ({ loadStartingItems, showFirstStoryMessage }) => {
@@ -34,13 +27,61 @@ const GameInstructions = ({ loadStartingItems, showFirstStoryMessage }) => {
             </div>
 
             <div className="character-creation__text">
+                <span style={{ paddingBottom: '0.5em' }}>
+                    {`Your Character's Name`}
+                </span>
+
                 <input
                     name="Character name"
                     type="text"
                     maxlength="512"
                     id="character-name"
+                    // className="character-creation__input"
+                    style={{
+                        height: '2.5em',
+                        backgroundColor: 'black',
+                        paddingLeft: '0.5em',
+                        paddingRight: '0.5em',
+                        marginLeft: '0.5em',
+                        marginRight: '0.5em',
+                    }}
+                    className="white-border"
                 />
+
+                <span style={{ paddingTop: 12 }}>{`Your Race`}</span>
+                <div className="container space-around">
+                    <Button
+                        title={'Human'}
+                        extraClass="character-creation__select-button"
+                    />
+                    <Button
+                        title={'Elf'}
+                        extraClass="character-creation__select-button"
+                    />
+                    <Button
+                        title={'Dwarf'}
+                        extraClass="character-creation__select-button"
+                    />
+                </div>
+
+                <span style={{ paddingTop: 12 }}>{`Your Class`}</span>
+                <div className="container space-around">
+                    <Button
+                        title={'Fighter'}
+                        extraClass="character-creation__select-button"
+                    />
+                    <Button
+                        title={'Wizard'}
+                        extraClass="character-creation__select-button"
+                    />
+                    <Button
+                        title={'Ranger'}
+                        extraClass="character-creation__select-button"
+                    />
+                </div>
             </div>
+
+            {/* name = document.getElementById("searchTxt").value; */}
 
             <div className="flex-column character-creation__button">
                 <Button onClick={handleContinue} title={'Continue'} />
