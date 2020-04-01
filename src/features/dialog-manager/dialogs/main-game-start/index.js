2ec2cdad468b7cd4ce4ee9b411f87d7b80d13cb2
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-04-01 22:03:11 +1300

    pages linked

--- src/features/dialog-manager/dialogs/main-game-start/index.js
+++ src/features/dialog-manager/dialogs/main-game-start/index.js
@@ -4,18 +4,18 @@ import { connect } from 'react-redux';
 import Button from '../../../../components/button';
 import Dialog from '../../../../components/dialog';
 import backToSelect from '../../actions/back-to-select';
-// import startMainGame from '../../../world/actions/start-main-game';
+import startMainGame from '../../../world/actions/start-main-game';
 import characterCreation from '../character-creation';
 //src\features\dialog-manager\dialogs\main-game-start\index.js
 //src\features\dialog-manager\dialogs\character-creation\index.js
 
 import './styles.scss';
 
-// const MainGameStart = ({ startMainGame, backToSelect }) => {
-const MainGameStart = ({ characterCreation, backToSelect }) => {
+const MainGameStart = ({ startMainGame, backToSelect }) => {
+    // const MainGameStart = ({ characterCreation, backToSelect }) => {
     return (
-        // <Dialog goBack={backToSelect} onKeyPress={startMainGame}>
-        <Dialog goBack={backToSelect} onKeyPress={characterCreation}>
+        <Dialog goBack={backToSelect} onKeyPress={startMainGame}>
+            {/* <Dialog goBack={backToSelect} onKeyPress={characterCreation}> */}
             <span className="flex-row game-start__title">{'Story Mode'}</span>
 
             <span className="flex-column game-start__text">
@@ -24,8 +24,8 @@ const MainGameStart = ({ characterCreation, backToSelect }) => {
 
             <div className="flex-column game-start__button">
                 <Button
-                    // onClick={startMainGame}
-                    onClick={characterCreation}
+                    onClick={startMainGame}
+                    // onClick={characterCreation}
                     icon="compass"
                     title={'Explore Dungeon'}
                 />
@@ -34,7 +34,7 @@ const MainGameStart = ({ characterCreation, backToSelect }) => {
     );
 };
 
-// const actions = { backToSelect, startMainGame };
-const actions = { backToSelect, characterCreation };
+const actions = { backToSelect, startMainGame };
+// const actions = { backToSelect, characterCreation };
 
 export default connect(null, actions)(MainGameStart);
