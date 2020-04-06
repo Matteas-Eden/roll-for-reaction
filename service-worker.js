b51322e719c3e11a2508428d191c9a1ca3962780
Pyxxil <12526499+Pyxxil@users.noreply.github.com>
2020-04-06 06:12:05 +0000

    Deploying to gh-pages from  - 866bd329092bf3a45e576935bb127656725345c2 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/roll-for-reaction/precache-manifest.b0fb2e5546ce38956d371e0a32f0c2a2.js"
+  "/roll-for-reaction/precache-manifest.94dc8b15bda01a0a61c1222552170781.js"
 );
 
 workbox.clientsClaim();
