a6df0fadcd7b07e4453d6f33b2079354adb5c71e
Josh Hill <josh.hill189@gmail.com>
2020-04-07 08:02:40 +1200

    Fix Exported Function Name

--- src/features/dialog-manager/actions/confirm-ability-score-dialog.js
+++ src/features/dialog-manager/actions/confirm-ability-score-dialog.js
@@ -1,4 +1,4 @@
-export default function confirmAbilityDialog() {
+export default function confirmAbilityScoreDialog() {
     return (dispatch, getState) => {
         const {
             constitution,
