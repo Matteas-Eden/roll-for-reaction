953c0d6764d20a3fd6f28d7392c4f9bfd1cea69a
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-04-15 22:19:12 +1200

    Add 1 to constitution bonus

--- src/utils/calculate-max-hp-pool.js
+++ src/utils/calculate-max-hp-pool.js
@@ -3,6 +3,6 @@ import { BASE_HEALTH } from '../config/constants';
 export default function calculateMaxHpPool(level, constitutionBonus) {
     return (
         BASE_HEALTH +
-        (level - 1) * (constitutionBonus > 0 ? constitutionBonus : 0)
+        (level - 1) * (constitutionBonus > 0 ? constitutionBonus + 1 : 0)
     );
 }
