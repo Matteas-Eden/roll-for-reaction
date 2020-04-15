a548be3f112d096125687a6419f7426093de5df3
Matteas-Eden <45587386+Matteas-Eden@users.noreply.github.com>
2020-04-15 11:53:07 +0000

    Deploying to gh-pages from  - 09597722dfb72df0a85c420ad05d4ccc4016f6ed 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/roll-for-reaction/precache-manifest.a03423d957545b3f01edd815fd5e5a63.js"
+  "/roll-for-reaction/precache-manifest.4606f23246034a14f24b22b3b8e42810.js"
 );
 
 workbox.clientsClaim();
