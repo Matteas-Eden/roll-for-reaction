03964263202fb6d81fb084478726c2e400b7eb98
Josh Hill <josh.hill189@gmail.com>
2020-04-20 10:53:02 +1200

    Add Damage Range to Item View

--- src/utils/dice.js
+++ src/utils/dice.js
@@ -1,11 +1,19 @@
-const roll = sides => {
-    return Math.floor(Math.random() * sides) + 1;
+const unbiased = () => {
+    return sides => Math.floor(Math.random() * sides) + 1;
+};
+
+const biased = to => {
+    if (to === 'max') {
+        return sides => sides;
+    } else if (to === 'min') {
+        return _ => 1;
+    }
 };
 
 const ops = {
     '+': {
         precedence: 1,
-        op: (left, right) => {
+        op: (left, right, _) => {
             if (Array.isArray(left)) {
                 left = left.reduce((sum, value) => sum + value);
             }
@@ -17,7 +25,7 @@ const ops = {
     },
     '-': {
         precedence: 1,
-        op: (left, right) => {
+        op: (left, right, _) => {
             if (Array.isArray(left)) {
                 left = left.reduce((sum, value) => sum + value);
             }
@@ -29,7 +37,7 @@ const ops = {
     },
     '*': {
         precedence: 2,
-        op: (left, right) => {
+        op: (left, right, _) => {
             if (Array.isArray(left)) {
                 left = left.reduce((sum, value) => sum + value);
             }
@@ -41,7 +49,7 @@ const ops = {
     },
     '/': {
         precedence: 2,
-        op: (left, right) => {
+        op: (left, right, _) => {
             if (Array.isArray(left)) {
                 left = left.reduce((sum, value) => sum + value);
             }
@@ -53,26 +61,26 @@ const ops = {
     },
     l: {
         precedence: 3,
-        op: (left, right) => {
+        op: (left, right, _) => {
             // Remove the lowest `right` number of rolls
             return left.sort().splice(right);
         },
     },
     h: {
         precedence: 3,
-        op: (left, right) => {
+        op: (left, right, _) => {
             // Select the highest `right` number of rolls
             return left.sort((l, r) => r - l).splice(right);
         },
     },
     d: {
         precedence: 4,
-        op: (left, right) => {
+        op: (left, right, die) => {
             let mul = parseInt(left);
             let sides = parseInt(right);
             let rolls = [];
             for (let i = 0; i < mul; i++) {
-                rolls.push(roll(sides));
+                rolls.push(die(sides));
             }
             return rolls;
         },
@@ -109,14 +117,14 @@ let yard = infix => {
         .join(' ');
 };
 
-const rpn = postfix => {
+const rpn = (postfix, die) => {
     let stack = [];
 
     postfix.split(' ').forEach(token => {
         if (token in ops) {
             let right = stack.pop();
             let left = stack.pop();
-            stack.push(ops[token].op(left, right));
+            stack.push(ops[token].op(left, right, die));
         } else {
             stack.push(token);
         }
@@ -125,8 +133,15 @@ const rpn = postfix => {
     return stack[0];
 };
 
+export const calculateDamageRange = notation => {
+    const pf = yard(notation);
+    const min = rpn(pf, biased('min'));
+    const max = rpn(pf, biased('max'));
+    return [min, max];
+};
+
 // Calculates damage to deal based on Dice Notation (https://en.wikipedia.org/wiki/Dice_notation)
 export default function calculateDamage(notation) {
-    const damage = rpn(yard(notation));
+    const damage = rpn(yard(notation), unbiased());
     return damage;
 }
