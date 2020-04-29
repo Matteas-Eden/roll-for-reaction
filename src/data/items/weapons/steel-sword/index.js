050f2d36e0dcebe98f8089b7524a333ad3253d4e
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-04-30 00:38:35 +1200

    Resize sprites to 40px

--- src/data/items/weapons/steel-sword/index.js
+++ src/data/items/weapons/steel-sword/index.js
@@ -3,6 +3,7 @@ import SteelSwordImg from './steel-sword.png';
 const SteelSword = {
     name: 'Steel Sword',
     type: 'weapon',
+    effect: {},
     range: 'melee',
     damage: '1d8 + 2',
     image: SteelSwordImg,
