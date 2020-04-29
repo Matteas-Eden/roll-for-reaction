56448d098c1c467ee903a983bc9f5f32996f049b
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2020-04-30 10:59:12 +1200

    Remove outdated comments

--- src/utils/calculate-prices.js
+++ src/utils/calculate-prices.js
@@ -4,11 +4,6 @@ import {
     MAX_PRICE_PERCENT,
 } from '../config/constants';
 
-// Calculates buy and sell price of an item
-// Params
-//  min: minimum percentage of base price that item will sell for
-//  mid: max sell percentage, min buy percentage
-//  max: maximum percentage of base price that item can be bought for
 export default function calculatePrices(basePrice, charismaModifier) {
     const buyPriceStep = (MID_PRICE_PERCENT - MAX_PRICE_PERCENT) / 10;
     const sellPriceStep = (MID_PRICE_PERCENT - MIN_PRICE_PERCENT) / 10;
