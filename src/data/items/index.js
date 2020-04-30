5e5e439d66c77144c0b04e8822c4c6dbac1d72ec
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-04-30 14:41:08 +1200

    Add ranged weapons

--- src/data/items/index.js
+++ src/data/items/index.js
@@ -9,6 +9,11 @@ import SteelBoots from './clothes/armor/steel-boots';
 import SteelHelm from './clothes/armor/steel-helm';
 import SteelGloves from './clothes/armor/steel-gloves';
 import SteelPants from './clothes/armor/steel-pants';
+import BlackRobes from './clothes/robes/black-robes';
+import BrownRobes from './clothes/robes/brown-robes';
+import RedRobes from './clothes/robes/red-robes';
+import TealRobes from './clothes/robes/teal-robes';
+import SkullRobes from './clothes/robes/skull-robes';
 import DiamondRing from './rings/diamond-ring';
 import AmethystRing from './rings/amethyst-ring';
 import OldRing from './rings/old-ring';
@@ -17,54 +22,63 @@ import RustySword from './weapons/swords/rusty-sword';
 import SteelSword from './weapons/swords/steel-sword';
 import DragonsBane from './weapons/swords/dragons-bane';
 import LichBane from './weapons/swords/lich-bane';
-import HpPotion from './potions/hp-potion';
-import GreatHpPotion from './potions/great-hp-potion';
-import ManaPotion from './potions/mana-potion';
-import GreatManaPotion from './potions/great-mana-potion';
-import BlackRobes from './clothes/robes/black-robes';
-import BrownRobes from './clothes/robes/brown-robes';
-import RedRobes from './clothes/robes/red-robes';
-import TealRobes from './clothes/robes/teal-robes';
-import SkullRobes from './clothes/robes/skull-robes';
 import BlackStave from './weapons/staves/black-stave';
 import BrownStave from './weapons/staves/brown-stave';
 import RedStave from './weapons/staves/red-stave';
 import TealStave from './weapons/staves/teal-stave';
 import SkullStave from './weapons/staves/skull-stave';
+import Boomerang from './weapons/ranged/boomerang';
+import Slingshot from './weapons/ranged/slingshot';
+import Bow from './weapons/ranged/bow';
+import Crossbow from './weapons/ranged/crossbow';
+import HpPotion from './potions/hp-potion';
+import GreatHpPotion from './potions/great-hp-potion';
+import ManaPotion from './potions/mana-potion';
+import GreatManaPotion from './potions/great-mana-potion';
 
 const items = {
     weapons: {
-        BroadSword,
-        RustySword,
-        SteelSword,
-        DragonsBane,
-        LichBane,
+        swords: {
+            BroadSword,
+            RustySword,
+            SteelSword,
+            DragonsBane,
+            LichBane,
+        },
+        ranged: {
+            Boomerang,
+            Slingshot,
+            Bow,
+            Crossbow,
+        },
+        staves: {
+            BlackStave,
+            BrownStave,
+            RedStave,
+            TealStave,
+            SkullStave,
+        },
     },
-    staves: {
-        BlackStave,
-        BrownStave,
-        RedStave,
-        TealStave,
-        SkullStave,
-    },
-    armor: {
-        LeatherArmor,
-        LeatherBoots,
-        LeatherCap,
-        LeatherGloves,
-        LeatherPants,
-        SteelArmor,
-        SteelBoots,
-        SteelHelm,
-        SteelGloves,
-        SteelPants,
-    },
-    robes: {
-        BlackRobes,
-        BrownRobes,
-        RedRobes,
-        TealRobes,
-        SkullRobes,
+    clothes: {
+        armor: {
+            LeatherArmor,
+            LeatherBoots,
+            LeatherCap,
+            LeatherGloves,
+            LeatherPants,
+            SteelArmor,
+            SteelBoots,
+            SteelHelm,
+            SteelGloves,
+            SteelPants,
+        },
+        robes: {
+            BlackRobes,
+            BrownRobes,
+            RedRobes,
+            TealRobes,
+            SkullRobes,
+        },
     },
     rings: {
         AmethystRing,
@@ -94,6 +108,8 @@ export const randomItemsT1 = [
     BrownRobes,
     BlackStave,
     BrownStave,
+    Boomerang,
+    Slingshot,
 ];
 
 export const randomItemsT2 = [
@@ -112,6 +128,8 @@ export const randomItemsT2 = [
     RedStave,
     TealStave,
     SkullStave,
+    Bow,
+    Crossbow,
 ];
 
 export default items;
