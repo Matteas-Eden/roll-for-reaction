052618322bd4b588e62073fbbfd29b9008cd560a
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-04-30 12:20:09 +1200

    Add mana potion

---
+++ src/data/items/potions/mana-potion/index.js
@@ -0,0 +1,12 @@
+import ManaPotionImg from './mana-potion.png';
+
+const ManaPotion = {
+    name: 'HP Potion',
+    type: 'potion',
+    mana: 15,
+    manaReset: 15,
+    image: ManaPotionImg,
+    value: 40,
+};
+
+export default ManaPotion;
