8a871c6afb3d13d648279b0b3cbf518934271fde
Frosty273 <kelvinngor387@gmail.com>
2020-05-03 21:28:31 +1200

    Add orc with armor sprite and stats

---
+++ src/data/monsters/orc-armor/index.js
@@ -0,0 +1,14 @@
+import OrcArmorSprite from './orc-armor.png';
+
+const OrcArmor = {
+    hp: 40,
+    maxHp: 40,
+    attackValue: 12,
+    defence: 5,
+    dice: '2d6',
+    exp: 280,
+    type: 'orcArmor',
+    sprite: OrcArmorSprite,
+};
+
+export default OrcArmor;
