798e183c5888992ab54396dc4481eef17e0ba582
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-05-03 18:57:19 +1200

    Add tiers up to Tier 4

--- src/components/shop-inventory/shop-items.js
+++ src/components/shop-inventory/shop-items.js
@@ -1,24 +1,45 @@
-import items, { randomItemsT1, randomItemsT2 } from '../../data/items';
-import { TIER_2 } from '../../config/constants';
+import items, {
+    randomItemsT1,
+    randomItemsT2,
+    randomItemsT3,
+    randomItemsT4,
+} from '../../data/items';
+import { TIER_2, TIER_3, TIER_4 } from '../../config/constants';
+
+const purchaseOnlyT1 = [
+    items.other.BackpackUpgrade,
+    items.potions.HpPotion,
+    // items.potions.MpPotion,
+];
+
+const purchaseOnlyT2 = [
+    ...purchaseOnlyT1,
+    items.potions.GreatHpPotion,
+    // items.potions.GreatMpPotion,
+];
+
+const purchaseOnlyT3 = [
+    ...purchaseOnlyT2,
+    items.potions.MightyManaPotion,
+    // items.potions.MightyHpPotion,
+];
+
+const purchaseOnlyT4 = [
+    ...purchaseOnlyT3,
+    items.potions.DivineHpPotion,
+    // items.potions.DivineManaPotion,
+];
+
 // returns the correct tier of items depending on player level
 export default function shopItems(level) {
     if (level < TIER_2) {
-        return [
-            ...randomItemsT1,
-            items.other.BackpackUpgrade,
-            items.potions.HpPotion,
-            items.potions.GreatHpPotion,
-        ];
-    } else {
-        return [
-            ...randomItemsT2,
-            items.other.BackpackUpgrade,
-            items.potions.HpPotion,
-            items.potions.GreatHpPotion,
-            // items.potions.MightyHpPotion,
-            // items.potions.DivineHpPotion,
-            items.potions.MightyManaPotion,
-            items.potions.DivineManaPotion,
-        ];
+        return [...randomItemsT1, ...purchaseOnlyT1];
     }
+    if (level < TIER_3) {
+        return [...randomItemsT2, ...purchaseOnlyT2];
+    }
+    if (level < TIER_4) {
+        return [...randomItemsT3, ...purchaseOnlyT3];
+    }
+    return [...randomItemsT4, ...purchaseOnlyT4];
 }
