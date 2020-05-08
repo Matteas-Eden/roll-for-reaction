8ef59edfc657862785c0be2aa25aff5a293e8458
Frosty273 <kelvinngor387@gmail.com>
2020-05-08 13:05:16 +1200

    Add new monsters

--- src/data/monsters/index.js
+++ src/data/monsters/index.js
@@ -1,17 +1,33 @@
+import Bat from './bat';
+import Cerberus from './cerberus';
 import Dragon from './dragon';
 import Goblin from './goblin';
+import Ghost from './ghost';
 import Imp from './imp';
 import Lich from './lich';
+import Medusa from './medusa';
+import Orc from './orc';
+import OrcArmor from './orc-armor';
 import Rat from './rat';
 import StoneGolem from './stone-golem';
+import Warlock from './warlock';
+import Wolf from './wolf';
 
 const monsters = {
+    bat: Bat,
+    cerberus: Cerberus,
     dragon: Dragon,
     goblin: Goblin,
+    ghost: Ghost,
     imp: Imp,
     lich: Lich,
+    medusa: Medusa,
+    orc: Orc,
+    'orc-armor': OrcArmor,
     rat: Rat,
     'stone-golem': StoneGolem,
+    warlock: Warlock,
+    wolf: Wolf,
 };
 
 export default monsters;
