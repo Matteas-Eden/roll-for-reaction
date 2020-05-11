7136cc95f52a149d52c60b8a50ba1d570872d37b
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-05-11 21:41:30 +1200

    Create empty maps

---
+++ src/data/maps/5/1/index.js
@@ -0,0 +1,57 @@
+const tiles = [
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 9, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    [5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5],
+    [5, 5, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 5, 5, 5],
+    [5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5],
+    [5, 5, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 4, 5, 0, 5, 5, 5, 5],
+    [5, 5, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5],
+    [5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5],
+    [5, 5, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 5, 5, 5],
+    [5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 5, 5, 5, 5],
+    [5, 5, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 5, 5, 5],
+    [5, 5, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+]; //0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+const wallType = 'purple-pattern';
+
+const monsters = [
+    {
+        type: 'goblin',
+        position: [5, 6],
+    },
+    {
+        type: 'orc',
+        position: [9, 11],
+    },
+    {
+        type: 'orc',
+        position: [15, 5],
+    },
+    {
+        type: 'stone-golem',
+        position: [6, 12],
+    },
+];
+
+const stairs = {
+    down: '2_5',
+    up: '3_2',
+};
+
+const message = {
+    title:
+        '<> has defeated the Lich, but more adventure lies ahead with more monsters to be slain.',
+    body: `As <> delves deeper into the dungeon, the walls change tone yet again.It is almost as if magic itself is imbued into the very stones`,
+};
+
+export default {
+    tiles,
+    wallType,
+    monsters,
+    stairs,
+    message,
+};
---
+++ src/data/maps/5/5/index.js
@@ -0,0 +1,73 @@
+const tiles = [
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 0, 0, 0, 0, 0, 0],
+    [5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [5, 5, 0, 5, 5, 5, 5, 5, 1, 0, 1, 5, 5, 1, 0, 0, 0, 0, 0, 0],
+    [5, 5, 0, 5, 5, 5, 5, 5, 5, 0, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0],
+    [5, 5, 0, 5, 5, 5, 5, 5, 1, 0, 1, 5, 5, 5, 0, 0, 0, 0, 0, 0],
+    [5, 1, 0, 1, 5, 5, 5, 5, 4, 0, 4, 5, 5, 5, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 5, 5, 5, 4, 0, 4, 5, 5, 5, 5, 1, 0, 1, 5, 5],
+    [0, 0, 0, 0, 0, 5, 5, 5, 4, 4, 4, 5, 5, 5, 5, 5, 0, 5, 5, 5],
+    [0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 5, 5, 5],
+    [0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 5, 5, 5],
+    [0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 5, 5, 5],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5],
+    [5, 5, 5, 5, 5, 1, 5, 5, 5, 0, 5, 5, 5, 5, 0, 3, 0, 5, 5, 5],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 5, 5, 5, 5, 0, 0, 0, 5, 5, 5],
+]; //0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+const wallType = 'purple';
+
+const monsters = [
+    {
+        type: 'stone-golem',
+        position: [0, 12],
+    },
+    {
+        type: 'stone-golem',
+        position: [4, 12],
+    },
+    {
+        type: 'orc',
+        position: [0, 10],
+    },
+    {
+        type: 'stone-golem',
+        position: [4, 10],
+    },
+    {
+        type: 'stone-golem',
+        position: [18, 1],
+    },
+    {
+        type: 'stone-golem',
+        position: [14, 1],
+    },
+    {
+        type: 'orc',
+        position: [18, 8],
+    },
+    {
+        type: 'stone-golem',
+        position: [14, 8],
+    },
+];
+
+const stairs = {
+    down: '3_4',
+    up: '4_1',
+};
+
+const message = {
+    title: '<> is faced with a choice.  A short cut lies to their right.',
+    body:
+        'The sounds of many hungry foes echoes down the corridor, making the shortcut look more promising.  But what treasures might they miss if they took it?',
+};
+
+export default {
+    tiles,
+    wallType,
+    monsters,
+    stairs,
+    message,
+};
