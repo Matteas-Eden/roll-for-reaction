f1127b6a36f12045cb0014e78ecc2d488acbddbd
Frosty273 <kelvinngor387@gmail.com>
2020-05-16 00:40:07 +1200

    Alter to use constant values

--- src/utils/calculate-max-hp-pool.js
+++ src/utils/calculate-max-hp-pool.js
@@ -4,6 +4,6 @@ export default function calculateMaxHpPool(level, constitutionBonus) {
     let healthBonus =
         constitutionBonus >= 0
             ? level * (5 + constitutionBonus)
-            : level * MIN_HEALTH_BONUS - 2;
+            : level * MIN_HEALTH_BONUS - MIN_HEALTH_BONUS;
     return BASE_HEALTH + healthBonus;
 }
