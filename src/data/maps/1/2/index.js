3a87ced231739b7bc47cfe659da103dac636bea5
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-05-15 15:10:46 +1200

    Add map 4_1

--- src/data/maps/1/2/index.js
+++ src/data/maps/1/2/index.js
@@ -46,15 +46,9 @@ const stairs = {
     down: '1_1',
 };
 
-// const message = {
-//     title: '<> frowns and looks around the dull room.',
-//     body: `This was not the room <> was looking for...`,
-// };
-
 export default {
     tiles,
     wallType,
     monsters,
     stairs,
-    // message,
 };
