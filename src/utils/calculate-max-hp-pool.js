0a4fbd858387e626464b93137e4be313ba8cd5db
Frosty273 <kelvinngor387@gmail.com>
2020-05-15 20:32:12 +1200

    Alter function calc

--- src/utils/calculate-max-hp-pool.js
+++ src/utils/calculate-max-hp-pool.js
@@ -1,14 +1,9 @@
 import { BASE_HEALTH, MIN_HEALTH_BONUS } from '../config/constants';
 
 export default function calculateMaxHpPool(level, constitutionBonus) {
-    let healthBonus;
-    if (level === 1) {
-        healthBonus = constitutionBonus >= 0 ? 5 + constitutionBonus : 0;
-    } else {
-        healthBonus =
-            constitutionBonus >= 0
-                ? level * (5 + constitutionBonus)
-                : (level - 1) * MIN_HEALTH_BONUS;
-    }
+    let healthBonus =
+        constitutionBonus >= 0
+            ? level * (5 + constitutionBonus)
+            : level * MIN_HEALTH_BONUS - 2;
     return BASE_HEALTH + healthBonus;
 }
--- src/utils/calculate-max-mana-pool.js
+++ src/utils/calculate-max-mana-pool.js
@@ -1,17 +1,9 @@
 import { BASE_MANA, MIN_MANA_BONUS } from '../config/constants';
 
 export default function calculateMaxManaPool(level, intelligenceBonus) {
-    let manaBonus;
-    if (level === 1) {
-        manaBonus =
-            intelligenceBonus >= 0
-                ? (intelligenceBonus + 2) * MIN_MANA_BONUS
-                : 0;
-    } else {
-        manaBonus =
-            intelligenceBonus >= 0
-                ? level * (intelligenceBonus + 2) * MIN_MANA_BONUS
-                : (level - 1) * MIN_MANA_BONUS;
-    }
+    let manaBonus =
+        intelligenceBonus >= 0
+            ? level * (intelligenceBonus + 2) * MIN_MANA_BONUS
+            : level * MIN_MANA_BONUS - MIN_MANA_BONUS;
     return BASE_MANA + manaBonus;
 }
