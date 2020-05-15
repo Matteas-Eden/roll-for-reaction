1449d9ece277b18e8370eabadf2c2da4e5436934
Frosty273 <kelvinngor387@gmail.com>
2020-05-15 20:04:14 +1200

    Alter HP calculation

--- src/utils/calculate-max-hp-pool.js
+++ src/utils/calculate-max-hp-pool.js
@@ -1,9 +1,14 @@
 import { BASE_HEALTH, MIN_HEALTH_BONUS } from '../config/constants';
 
 export default function calculateMaxHpPool(level, constitutionBonus) {
-    return (
-        BASE_HEALTH +
-        (level - 1) *
-            (constitutionBonus >= 0 ? constitutionBonus + 2 : MIN_HEALTH_BONUS)
-    );
+    let healthBonus;
+    if (level === 1) {
+        healthBonus = constitutionBonus >= 0 ? 5 + constitutionBonus : 0;
+    } else {
+        healthBonus =
+            constitutionBonus >= 0
+                ? level * (5 + constitutionBonus)
+                : (level - 1) * MIN_HEALTH_BONUS;
+    }
+    return BASE_HEALTH + healthBonus;
 }
