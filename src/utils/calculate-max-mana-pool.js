ec0688f1434e59765ba7afd42369475f7593176e
Frosty273 <kelvinngor387@gmail.com>
2020-05-15 20:05:36 +1200

    Alter max mana calculation

--- src/utils/calculate-max-mana-pool.js
+++ src/utils/calculate-max-mana-pool.js
@@ -1,11 +1,17 @@
 import { BASE_MANA, MIN_MANA_BONUS } from '../config/constants';
 
 export default function calculateMaxManaPool(level, intelligenceBonus) {
-    return (
-        BASE_MANA +
-        (level - 1) *
-            (intelligenceBonus >= 0
+    let manaBonus;
+    if (level === 1) {
+        manaBonus =
+            intelligenceBonus >= 0
                 ? (intelligenceBonus + 2) * MIN_MANA_BONUS
-                : MIN_MANA_BONUS)
-    );
+                : 0;
+    } else {
+        manaBonus =
+            intelligenceBonus >= 0
+                ? level * (intelligenceBonus + 2) * MIN_MANA_BONUS
+                : (level - 1) * MIN_MANA_BONUS;
+    }
+    return BASE_MANA + manaBonus;
 }
