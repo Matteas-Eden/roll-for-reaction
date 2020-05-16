08c95a93f9cf53de2f8580fdab691437558cd6a9
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-05-16 12:02:43 +1200

    Weaken Lich

--- src/data/monsters/lich/index.js
+++ src/data/monsters/lich/index.js
@@ -3,9 +3,9 @@ import LichSprite from './lich.png';
 const Lich = {
     hp: 100,
     maxHp: 100,
-    attackValue: '3d20 + 20',
+    attackValue: '3d20 + 10',
     defence: 12,
-    dice: '2d20',
+    dice: '1d20 + 10',
     exp: 1000,
     type: 'lich',
     sprite: LichSprite,
