9a8db7e5f5e2360c6dc22329cbbcc42ca35fd594
Matt Eden <mrmatteden@gmail.com>
2020-05-17 21:03:55 +1200

    Finish combat page

--- src/features/dialog-manager/dialogs/tutorial-dialog/pages/combat-tutorial.js
+++ src/features/dialog-manager/dialogs/tutorial-dialog/pages/combat-tutorial.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
+import Spacebar from './assets/keyboard/space_key.png';
+import CKey from './assets/keyboard/c_key.png';
+
 import './styles.scss';
 
 const CombatTutorial = () => {
     return (
         <div className="flex-column tutorial-combat__container">
             <div className="tutorial-page__title">{'COMBAT'}</div>
+            <div className="tutorial-page__combat">
+                {
+                    "Attack your enemies with 'Spacebar' or throw spells at them with 'C'"
+                }{' '}
+                <br />
+                <div className="tutorial-page__combat-buttons">
+                    <img src={Spacebar} alt="spacebar" width={70} />
+                    <img src={CKey} alt="c key" width={70} /> <br />
+                </div>
+                {
+                    'To hit enemies, your attack must match or exceed their defence. Your attack is equal to a d20 + STR. Defence is a combination of your armour and DEX.'
+                }
+                <br />
+                {'A high defence value means you are less likely to get hit.'}
+            </div>
         </div>
     );
 };
