b9cfa9458398faa41e513f08f44c738d1faf6223
Matt Eden <mrmatteden@gmail.com>
2020-05-18 11:53:02 +1200

    Fix description for combat page

--- src/features/dialog-manager/dialogs/tutorial-dialog/pages/combat-tutorial.js
+++ src/features/dialog-manager/dialogs/tutorial-dialog/pages/combat-tutorial.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Spacebar from './assets/keyboard/space_key.png';
-import CKey from './assets/keyboard/c_key.png';
 
 import './styles.scss';
 
@@ -10,19 +9,13 @@ const CombatTutorial = () => {
         <div className="flex-column tutorial-combat__container">
             <div className="tutorial-page__title">{'COMBAT'}</div>
             <div className="tutorial-page__combat">
-                {
-                    "Attack your enemies with 'Spacebar' or throw spells at them with 'C'"
-                }{' '}
-                <br />
+                {"Attack your enemies with 'Spacebar'"} <br />
                 <div className="tutorial-page__combat-buttons">
-                    <img src={Spacebar} alt="spacebar" width={70} />
-                    <img src={CKey} alt="c key" width={70} /> <br />
+                    <img src={Spacebar} alt="spacebar" width={100} />
                 </div>
                 {
-                    'To hit enemies, your attack must match or exceed their defence. Your attack is equal to a d20 + STR. Defence is a combination of your armour and DEX.'
+                    'To hit enemies, your attack must match or exceed their defence. Your attack is equal to a d20 + STR/DEX/INT, depending on whether the attack is melee, ranged or spell. Defence is a combination of your armour and DEX.'
                 }
-                <br />
-                {'A high defence value means you are less likely to get hit.'}
             </div>
         </div>
     );
