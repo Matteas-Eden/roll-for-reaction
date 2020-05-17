a5a2857475c00c8ca8a54ca5b0f29e9650406f82
Matt Eden <mrmatteden@gmail.com>
2020-05-17 19:36:37 +1200

    Create spell tutorial page

--- src/features/dialog-manager/dialogs/tutorial-dialog/pages/spell-tutorial.js
+++ src/features/dialog-manager/dialogs/tutorial-dialog/pages/spell-tutorial.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
+import BKey from './assets/keyboard/b_key.png';
+import CKey from './assets/keyboard/c_key.png';
+
 import './styles.scss';
 
 const SpellTutorial = () => {
     return (
         <div className="flex-column tutorial-spell__container">
             <div className="tutorial-page__title">{'SPELL'}</div>
+            <div className="tutorial-page__spell">
+                {'Spells are powerful, but require mana.'}
+                <br />
+                <br />
+                {
+                    "Open the spellbook with 'B' or the button on the top-right of the screen."
+                }
+                <br />
+                <img src={BKey} alt="b key" width={75} />
+                <br />
+                {
+                    "Cast spells with 'C' or the button on the top-right of the screen."
+                }
+                <br />
+                <img src={CKey} alt="c key" width={75} />
+            </div>
         </div>
     );
 };
