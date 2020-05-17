e3952f3303b8145db082d14900d4e12bec9ea8b6
Matt Eden <mrmatteden@gmail.com>
2020-05-17 19:38:23 +1200

    Add new keyboard icons to movement page

--- src/features/dialog-manager/dialogs/tutorial-dialog/pages/movement-tutorial.js
+++ src/features/dialog-manager/dialogs/tutorial-dialog/pages/movement-tutorial.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
-import ArrowKeys from './assets/arrow-keys.png';
+import ArrowKeys from './assets/keyboard/arrow_keys.png';
 import DoubleTap from './assets/double-tap.png';
-import Enter from './assets/enter.png';
-import Space from './assets/space.png';
+import Space from './assets/keyboard/space_key.png';
 import Swipe from './assets/swipe.png';
-import WASDKeys from './assets/wasd-keys.png';
+import WASDKeys from './assets/keyboard/wasd_keys.png';
 
 import './styles.scss';
 
 const MovementTutorial = () => {
     return (
-        <div className="flex-column tutorial-movement__container">
+        <div className="flex-column tutorial-page__container">
             <div className="tutorial-page__title centered">{'MOVEMENT'}</div>
+            <div className="tutorial-page__movement">
+                {'Move the player with WASD'} <br /> <br />
+                <img src={WASDKeys} alt="wasd-keys" width={150} /> <br /> <br />
+                {'Or with the arrow keys'} <br /> <br />
+                <img src={ArrowKeys} alt="arrow-keys" width={150} />
+            </div>
         </div>
     );
 };
