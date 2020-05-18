0649bf92a7f777032aa65e43815f3641c41dbc4d
Matt Eden <mrmatteden@gmail.com>
2020-05-19 01:25:10 +1200

    Add tutorial buttons to game start screens
    As-of-yet unknown issue prevents the buttons from working

--- src/features/dialog-manager/dialogs/main-game-start/index.js
+++ src/features/dialog-manager/dialogs/main-game-start/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Button from '../../../../components/button';
 import Dialog from '../../../../components/dialog';
 import backToSelect from '../../actions/back-to-select';
+import toggleTutorial from '../../actions/toggle-tutorial';
 import startMainGame from '../../../world/actions/start-main-game';
 
 import './styles.scss';
@@ -16,6 +17,14 @@ const MainGameStart = ({ startMainGame, backToSelect }) => {
             <span className="flex-column game-start__text">
                 {'Explore the dark dungeon, full of monsters and gear!'}
             </span>
+
+            <div className="flex-column game-start__button">
+                <Button
+                    onClick={toggleTutorial}
+                    icon="question-circle"
+                    title={'How To Play'}
+                />
+            </div>
 
             <div className="flex-column game-start__button">
                 <Button
