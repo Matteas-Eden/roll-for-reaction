93b66aa9c5e291b6dafc07e2fc13f6221a29eac1
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-05-18 22:33:14 +1200

    Change double quotes to single quotes

--- src/data/maps/1/4/index.js
+++ src/data/maps/1/4/index.js
@@ -44,7 +44,7 @@ const stairs = {
 
 const message = {
     title: `<> thought back to all the monsters they've slain.`,
-    body: `'I hope someone else is cleaning up the blood, cause I'm not getting paid enough to do that...'`,
+    body: `"I hope someone else is cleaning up the blood, cause I'm not getting paid enough to do that..."`,
 };
 
 export default {
