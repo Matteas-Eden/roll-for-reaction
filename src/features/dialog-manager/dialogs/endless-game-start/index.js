c21574d10bca562338ca80a529392438ee8c953f
Matt Eden <mrmatteden@gmail.com>
2020-05-21 16:48:21 +1200

    Add help buttons to main game and endless game

--- src/features/dialog-manager/dialogs/endless-game-start/index.js
+++ src/features/dialog-manager/dialogs/endless-game-start/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Button from '../../../../components/button';
 import Dialog from '../../../../components/dialog';
 import backToSelect from '../../actions/back-to-select';
+import toggleTutorial from '../../actions/toggle-tutorial';
 import startEndlessGame from '../../../world/actions/start-endless-game';
 
 import './styles.scss';
@@ -20,6 +21,14 @@ const EndlessGameStart = ({ backToSelect, startEndlessGame }) => {
                     'Up for a challenge..? These randomly generated dungeons will run you into oblivion.'
                 }
             </span>
+
+            <div className="flex-column game-start__button">
+                <Button
+                    onClick={toggleTutorial}
+                    icon="question-circle"
+                    title={'How To Play'}
+                />
+            </div>
 
             <div className="flex-column endless-start__button">
                 <Button
