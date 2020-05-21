148ad49684b44276401bd375d00cc30060e45d03
Frosty273 <kelvinngor387@gmail.com>
2020-05-21 15:48:40 +1200

    Add new ammo

---
+++ src/data/ammo/pea/index.js
@@ -0,0 +1,11 @@
+import PeaImg from './pea.png';
+
+const Pea = {
+    name: 'pea',
+    target: 'enemy',
+    size: { width: 40, height: 40, total: 560 },
+    sprite: PeaImg,
+    information: 'shot a giant',
+};
+
+export default Pea;
