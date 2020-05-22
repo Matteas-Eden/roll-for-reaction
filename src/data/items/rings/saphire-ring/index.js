b144512695ceb89744f780f50dec9751d194df24
KimberleyEvans-Parker <kim@evans-parker.com>
2020-05-22 20:16:08 +1200

    Add saphire ring

---
+++ src/data/items/rings/saphire-ring/index.js
@@ -0,0 +1,13 @@
+import SaphireRingImg from './saphire-ring.png';
+
+const SaphireRing = {
+    name: 'Saphire Ring',
+    type: 'ring',
+    effect: {
+        defence: 10,
+    },
+    image: SaphireRingImg,
+    value: 150,
+};
+
+export default SaphireRing;
